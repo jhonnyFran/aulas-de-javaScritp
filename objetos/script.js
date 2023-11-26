f64bf1601ab8009013a3de5d66b8071b1cd4cde0
johnnyFran <costajhonny03@gmail.com>
2023-11-26 12:38:52 -0300

    vacilei e deixei em loop

--- objetos/script.js
+++ objetos/script.js
@@ -85,3 +85,4 @@ console.log(btn)
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+// vacilei e deixei em loop
