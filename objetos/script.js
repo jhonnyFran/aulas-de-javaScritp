fea463bae58400b69856f9619c1db6670f98aee5
johnnyFran <costajhonny03@gmail.com>
2023-11-26 12:32:20 -0300

    jhonny

--- objetos/script.js
+++ objetos/script.js
@@ -81,6 +81,7 @@ var btn = document.querySelector('.btn')
 // innerHTML
 // outerHTML
 console.log(btn)
+console.log(btn)
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
